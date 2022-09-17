db9598eab188e06d9a4fc996d8eea198a42f89b4
yosmo78 <yosmo78@gmail.com>
2022-09-17 15:51:40 -0700

    update build script

--- Code/Compile.bat
+++ Code/Compile.bat
@@ -3,10 +3,13 @@ if not defined DevEnvDir (
 	call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 )
 
-set FILES=main.cpp
+set FILES=..\Code\main.cpp
 
 set LIBS=opengl32.lib kernel32.lib user32.lib gdi32.lib
 
-cl /nologo /W3 /Z7 /GS- /Gs999999 /O2  /arch:AVX2 /DMAIN_DEBUG=0 %FILES% -Fe..\bin\TheButton.exe %LIBS% /link /incremental:no /opt:icf /opt:ref /subsystem:windows
 
-cl /nologo /W3 /Z7 /GS- /Gs999999 /arch:AVX2 /DMAIN_DEBUG=1 %FILES% -Fe..\bin\TheButtonDebug.exe %LIBS% /link /incremental:no /opt:icf /opt:ref /subsystem:console
+IF NOT EXIST ..\bin mkdir ..\bin
+pushd ..\bin
+cl /MT /nologo /W3 /Z7 /GS- /Gs999999 /O2  /arch:AVX2 /DMAIN_DEBUG=0 %FILES% -Fe..\bin\TheButton.exe %LIBS% /link /incremental:no /opt:icf /opt:ref /subsystem:windows
+cl /MTd /nologo /W3 /Z7 /GS- /Gs999999 /arch:AVX2 /DMAIN_DEBUG=1 %FILES% -Fe..\bin\TheButtonDebug.exe %LIBS% /link /incremental:no /opt:icf /opt:ref /subsystem:console
+popd
